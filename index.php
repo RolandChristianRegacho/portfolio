87b2d13a4a870161686d34086341d59d00a126ed
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-11-18 11:22:22 +0800

    First Release

--- index.php
+++ index.php
@@ -18,8 +18,12 @@
         <div class = "navbar">
             <button onclick = "goTo('home');"><i class="fa-solid fa-house"></i> Home</button>
             <button onclick = "goTo('about');"><i class="fa-solid fa-circle-info"></i> About</button>
-            <button><i class="fa-solid fa-folder-open"></i> Projects</button>
-            <button><i class="fa-solid fa-address-card"></i> Contact</button>
+            <button onclick = "goTo('project');"><i class="fa-solid fa-folder-open"></i> Projects</button>
+            <button onclick = "goTo('contact');"><i class="fa-solid fa-address-card"></i> Contact</button>
+        </div>
+        <div class = "icon">
+            <mark>Too</mark>
+            <img src = "design/icon/black-in-white-512.png" />
         </div>
         <div class = "home" id = "home">
             <br>
@@ -31,14 +35,16 @@
             <h3>Hi!</h3>
             <h1>I'm Roland</h1>
             <h2>Junior Web Developer</h2>
-            <button><i class="fa-brands fa-twitter"></i></button>
-            <button><i class="fa-brands fa-facebook"></i></button>
-            <button><i class="fa-brands fa-google"></i></button>
-            <button><i class="fa-brands fa-skype"></i></button>
-            <button><i class="fa-solid fa-phone"></i></button>
+            <button onclick = "goTo('twitter');"><i class="fa-brands fa-twitter"></i></button>
+            <button onclick = "goTo('facebook');"><i class="fa-brands fa-facebook"></i></button>
+            <button onclick = "goTo('contact');"><i class="fa-brands fa-google"></i></button>
+            <button onclick = "goTo('contact');"><i class="fa-brands fa-skype"></i></button>
+            <button onclick = "goTo('contact');"><i class="fa-solid fa-phone"></i></button>
         </div>
         <div class = "about" id = "about">
             <div class = "left">
+                <br>
+                <br>
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;An IT Graduate with exceptional strengths in handling stressful situations and has great critical problem solving ability. I also love developing small web applications to test my current skills in web development. I've already done a simple game that you can see in my portfolio.</p>
             </div>
             <div class = "right">
@@ -60,6 +66,77 @@
                     </tbody>
                 </table>
             </div>
+        </div>
+        <div class = "project" id = "project">
+            <br>
+            <br>
+            <br>
+            <h1>Projects</h1>
+            <div id = "click_titans" onmouseenter = "showDescription(this);" onmouseleave = "hideDescription(this);" class = "project-item">
+                <div id = "click_titans-show" class = "project-show">
+                    <div class = "project-icon c-red"><i class="fa-solid fa-gamepad"></i></div>
+                    <div class = "project-title">Click Titans</div>
+                </div>
+                <div id = "click_titans-hidden" class = "project-hidden">
+                    <div class = "project-description"><p>A simple clicking game resembles tap titans made using HTML, Native Js, JQuery and PHP.</p></div>
+                    <div class = "project-button"><button onclick = "goTo('clicktitans');">Visit</button></div>
+                </div>
+            </div>
+            <div id = "portal" onmouseenter = "showDescription(this);" onmouseleave = "hideDescription(this);" class = "project-item">
+                <div id = "portal-show" class = "project-show">
+                    <div class = "project-icon c-yellow"><i class="fa-solid fa-address-card"></i></div>
+                    <div class = "project-title">GradePortal</div>
+                </div>
+                <div id = "portal-hidden" class = "project-hidden">
+                    <div class = "project-description"><p>A Grading Portal App for Elementary and High School in St. Mark’s College.</p></div>
+                    <div class = "project-button"><button onclick = "goTo('gradeportal');">Visit</button></div>
+                </div>
+            </div>
+            <div id = "pos" onmouseenter = "showDescription(this);" onmouseleave = "hideDescription(this);" class = "project-item">
+                <div id = "pos-show" class = "project-show">
+                    <div class = "project-icon c-blue"><i class="fa-solid fa-receipt"></i></div>
+                    <div class = "project-title">Onepos</div>
+                </div>
+                <div id = "pos-hidden" class = "project-hidden">
+                    <div class = "project-description"><p>A POS app made using HTML, Native Js, JQuery and PHP.</p></div>
+                    <div class = "project-button"><button onclick = "goTo('pos');">Visit</button></div>
+                </div>
+            </div>
+        </div>
+        <div class = "contact" id = "contact">
+            <br>
+            <br>
+            <br>
+            <h1>Contact Info</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td><i class="fa-brands fa-twitter"></i></td>
+                        <td>@TooOneFor</td>
+                        <td><button onclick = "goTo('twitter');">Visit</button></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-brands fa-facebook"></i></td>
+                        <td>Roland Christian Too Regacho</td>
+                        <td><button onclick = "goTo('facebook');">Visit</button></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-brands fa-google"></i></td>
+                        <td id = "email">regacho800@gmail.com</td>
+                        <td><button onclick = "copyElement('email');">Copy</button></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-brands fa-skype"></i></td>
+                        <td id = "skype">live:.cid.8691244394ae269b</td>
+                        <td><button onclick = "copyElement('skype');">Copy</button></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa-solid fa-phone"></i></td>
+                        <td id = "phone">+639215382233</td>
+                        <td><button onclick = "copyElement('phone');">Copy</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </body>
 </html>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,114 @@ $(() => {
 })
 
 function goTo(form) {
+    let pixels = 0
+    if(form == "about") {
+        pixels = 600
+    }
+    if(form == "project") {
+        pixels = 1200
+    }
+    if(form == "contact") {
+        var body = document.body
+        var height = Math.max( body.scrollHeight, body.offsetHeight);
+        pixels = height - 600
+    }
+    if(form == "facebook") {
+        window.open("https://www.facebook.com/rolandchristiantoo.regacho/")
+        return
+    }
+    if(form == "twitter") {
+        window.open("https://twitter.com/TooOneFor")
+        return
+    }
+    if(form == "clicktitans") {
+        window.open("https://github.com/naseGaming/ClickTitans")
+        return
+    }
+    if(form == "gradeportal") {
+        window.open("https://github.com/naseGaming/onlineGrading")
+        return
+    }
+    if(form == "pos") {
+        window.open("https://1pos.1teqproviders.com.ph/")
+        return
+    }
     $('body').animate({
-        scrollTop: $("#" + form).offset().top
+        scrollTop: pixels
     }, 1000);
+}
+
+function showDescription(form) {
+    const id = form.id
+
+    $("#" + id + "-show").hide()
+    $("#" + id + "-hidden").show()
+}
+
+function hideDescription(form) {
+    const id = form.id
+
+    $("#" + id + "-hidden").hide()
+    $("#" + id + "-show").show()
+}
+
+function copyElement(data) {
+    if(copyToClipboard(document.getElementById(data))) {
+        console.log("copied")
+    }
+    else {
+        console.log("copy failed")
+    }
+}
+
+
+//copy to clipboard function https://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard/22581382#22581382
+function copyToClipboard(elem) {
+    // create hidden text element, if it doesn't already exist
+  var targetId = "_hiddenCopyText_";
+  var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+  var origSelectionStart, origSelectionEnd;
+  if (isInput) {
+      // can just use the original source element for the selection and copy
+      target = elem;
+      origSelectionStart = elem.selectionStart;
+      origSelectionEnd = elem.selectionEnd;
+  } else {
+      // must use a temporary form element for the selection and copy
+      target = document.getElementById(targetId);
+      if (!target) {
+          var target = document.createElement("textarea");
+          target.style.position = "absolute";
+          target.style.left = "-9999px";
+          target.style.top = "0";
+          target.id = targetId;
+          document.body.appendChild(target);
+      }
+      target.textContent = elem.outerText;
+  }
+  // select the content
+  var currentFocus = document.activeElement;
+  target.focus();
+  target.setSelectionRange(0, target.value.length);
+  
+  // copy the selection
+  var succeed;
+  try {
+        succeed = document.execCommand("copy");
+  } catch(e) {
+      succeed = false;
+  }
+  // restore original focus
+  if (currentFocus && typeof currentFocus.focus === "function") {
+      currentFocus.focus();
+  }
+  
+  if (isInput) {
+      // restore prior selection
+      elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+  } else {
+      // clear temporary content
+      target.textContent = "";
+  }
+  return succeed;
 }
